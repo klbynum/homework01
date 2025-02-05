9b635382a58d17fe3886dcb2ea892f782ad57b13
Kemon Bynum <monbynum51@icloud.com>
2025-02-05 16:51:59 -0500

    Update studentRecords.py

--- M2/studentRecords.py
+++ M2/studentRecords.py
@@ -8,28 +8,33 @@ def read_file():
     for line in lines:
         stuName, stuID, courseName, courseGrade = line.strip().split(", ")
         print(f"Name: {stuName}, ID: {stuID}, Course: {courseName}, Grade: {courseGrade} ")
-    print(stuName)
 
     # close the file
     file.close() 
     
-    return
+    return stuName, stuID, courseName, courseGrade
+
 # add a student record func()
 def add_record():
     # write to file
     return
+
 # print all student records func()
 def print_students():
     return
+
 # search for a student func()
 def search_student():
     return
+
 # update student grade func()
 def update_grade():
     return
+
 # delete a student record func()
 def delete_student():
     return
+
 # main func()
 def main():
     read_file()
