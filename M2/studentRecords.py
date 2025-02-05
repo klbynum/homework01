4b568a67ed636c294eba8a7390d631bc732ef8a3
Kemon Bynum <monbynum51@icloud.com>
2025-02-05 13:58:57 -0500

    Update studentRecords.py

---
+++ M2/studentRecords.py
@@ -0,0 +1,38 @@
+def read_file():
+    # open file
+    file = open('M2/grades.txt', 'r')
+
+    # read the file
+    # content = file.read()
+
+    # print(content)
+    lines = file.readlines()
+
+    for line in lines:
+        print(line)
+
+    # close the file
+    file.close() 
+    return
+# add a student record func()
+def add_record():
+    return
+# print all student records func()
+def print_students():
+    return
+# search for a student func()
+def search_student():
+    return
+# update student grade func()
+def update_grade():
+    return
+# delete a student record func()
+def delete_student():
+    return
+# main func()
+def main():
+    read_file()
+    return
+
+if __name__ == "__main__":
+    main()
