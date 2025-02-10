1a793dd692c241e272ea667dc7176c77a8e6abbe
Kemon <kemonbynum18@gmail.com>
2025-02-10 10:50:31 -0500

    added code

--- M2/studentRecords.py
+++ M2/studentRecords.py
@@ -1,44 +1,105 @@
-def read_file():
+def read_file(): # WORKS!
     # open file
     file = open('M2/grades.txt', 'r')
-
     # read the file
     lines = file.readlines()
-
+    # Put students in a list
+    students = []
     for line in lines:
         stuName, stuID, courseName, courseGrade = line.strip().split(", ")
-        print(f"Name: {stuName}, ID: {stuID}, Course: {courseName}, Grade: {courseGrade} ")
-
+        students.append((stuName, stuID, courseName, courseGrade))
     # close the file
     file.close() 
-    
-    return stuName, stuID, courseName, courseGrade
+    return students
 
-# add a student record func()
-def add_record():
-    # write to file
-    return
+def write_file(students):
+    with open('M2/grades.txt', 'w') as file:
+        for stuName, stuID, courseName, courseGrade in students:
+            file.write(f"{stuName}, {stuID}, {courseName}, {courseGrade} \n")
+        file.close()
 
-# print all student records func()
-def print_students():
-    return
+# add a student record func() WORKS!
+def add_record(students):
+    stuName = input("Enter student name: ")
+    stuID = input("Enter student ID: ")
+    courseName = input("Enter course name: ")
+    courseGrade = input("Enter course grade: ")
+    # add student to the list
+    students.append((stuName, stuID, courseName, courseGrade))
+    print("Student has been added.\n")
+    print_students(students)
+
+# print all student records func() WORKS!
+def print_students(students):
+    print("All Students:")
+    for stuName, stuID, courseName, courseGrade in students:
+        print(f"Student Name: {stuName}, Student ID: {stuID}, Course Name: {courseName}, Course Grade: {courseGrade} \n")
 
 # search for a student func()
-def search_student():
-    return
+def search_student(students, stuSearch):
+    found = False
+    with open('M2/grades.txt', 'r') as file: 
+        lines = file.readlines()
+        for stuName, stuID, courseName, courseGrade in students: 
+            if stuID == stuSearch:
+                print(f"Student Found: Name {stuName}, ID: {stuID}, Course {courseName}, Grade: {courseGrade}\n")
+                found = True
+                break
+    if not found:
+        print("No record for student found")
 
 # update student grade func()
-def update_grade():
-    return
+def update_grade(students, stuSearch):
+    newGrade = input("Enter a new grade for student: ")
+    updated = False
+    search_student(students, stuSearch)
+
 
 # delete a student record func()
-def delete_student():
-    return
+def delete_student(students, stuSearch):
+    deleted = False
+    # Remove the record
 
 # main func()
 def main():
-    read_file()
-    return
+    students = read_file()
+    print("Enter 1 to add student record")
+    print("Enter 2 to view all student records")
+    print("Enter 3 to for a student by ID")
+    print("Enter 4 to update student record")
+    print("Enter 5 to delete a student record")
+    print("Enter 0 to exit")
+    choice = input("Enter your choice: ")
+    print("\n")
+
+    while choice != '0':
+       if choice == '1':
+           add_record(students)
+       elif choice == '2':
+           print_students(students)
+       elif choice == '3':
+           stuSearch = input("Enter the student ID: ")
+           search_student(students, stuSearch)
+       elif choice =='4':
+           stuSearch = input("Enter the student ID: ")
+           search_student(students, stuSearch)
+           update_grade(students, stuSearch)
+       elif choice == '5':
+           stuSearch = input("Enter the ID of student to delete: ")
+           search_student(students, stuSearch)
+           delete_student(students, stuSearch)
+       elif choice == '0':
+           print("Exiting...")
+           break
+       else:
+           print("This is not an option. Please try again.")
+       print("Enter 1 to add student record")
+       print("Enter 2 to view all student records")
+       print("Enter 3 to for a student by ID")
+       print("Enter 4 to update student record")
+       print("Enter 5 to delete a student record")
+       print("Enter 0 to exit")
+       choice = input("Enter your choice: ")
 
 if __name__ == "__main__":
     main()
